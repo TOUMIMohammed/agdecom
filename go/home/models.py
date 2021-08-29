4822ca2f70de54d835a4289fce02b24ede4eaad7
Sami <sami@ezz.com>
2021-08-29 21:29:36 +0000

    working PO

--- go/home/models.py
+++ go/home/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class globalConfig(models.Model):
+    name = models.CharField(max_length=200,default="Tropical Deliveries")
+    year = models.CharField(max_length=200,default="2021")
+    title = models.CharField(max_length=200)
+    website = models.CharField(max_length=200)
+    email = models.CharField(max_length=200)
+    def __str__(self):
+        return self.title
--- go/home/views.py
+++ go/home/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from .models import globalConfig
+
+
 
 # Create your views here.
+def startPage(request):
+    return HttpResponse("Hello, world. You're at the home index.")
+
+def index(request):
+    configTable = globalConfig.objects.all()
+    if configTable:
+        config = configTable[0]
+    else:
+        config=[]
+    #all_product_list = Product.objects.all()
+    template = loader.get_template('home/index.html')
+    context = {
+        #'all_product_list': all_product_list,
+        'config': config,
+    }
+    return HttpResponse(template.render(context, request))

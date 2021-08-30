01bc6ccf0b29ef96af6d73fae43dbd6e3f3d89a9
Sami <sami@ezz.com>
2021-08-30 01:32:08 +0000

    zorking wb

--- go/home/urls.py
+++ go/home/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('home/', views.index, name='index'),
+    path('contact/', views.contact, name='contact'),
+    path('about/', views.about, name='about'),
+    path('author/', views.author, name='author'),
+    path('faq/', views.faq, name='faq'),
+    path('pricing/', views.pricing, name='pricing'),
+    path('login/', views.login, name='login'),
 ]
--- go/home/views.py
+++ go/home/views.py
@@ -22,3 +22,82 @@ def index(request):
         'config': config,
     }
     return HttpResponse(template.render(context, request))
+
+
+def contact(request):
+    configTable = globalConfig.objects.all()
+    if configTable:
+        config = configTable[0]
+    else:
+        config=[]
+    #all_product_list = Product.objects.all()
+    template = loader.get_template('home/pages/contact-us.html')
+    context = {
+        #'all_product_list': all_product_list,
+        'config': config,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def about(request):
+    configTable = globalConfig.objects.all()
+    if configTable:
+        config = configTable[0]
+    else:
+        config=[]
+    template = loader.get_template('home/pages/about-us.html')
+    context = {
+        'config': config,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def author(request):
+    configTable = globalConfig.objects.all()
+    if configTable:
+        config = configTable[0]
+    else:
+        config=[]
+    template = loader.get_template('home/pages/author.html')
+    context = {
+        'config': config,
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def faq(request):
+    configTable = globalConfig.objects.all()
+    if configTable:
+        config = configTable[0]
+    else:
+        config=[]
+    template = loader.get_template('home/pages/faq.html')
+    context = {
+        'config': config,
+    }
+    return HttpResponse(template.render(context, request))
+
+def pricing(request):
+    configTable = globalConfig.objects.all()
+    if configTable:
+        config = configTable[0]
+    else:
+        config=[]
+    template = loader.get_template('home/pages/pricing.html')
+    context = {
+        'config': config,
+    }
+    return HttpResponse(template.render(context, request))
+
+def login(request):
+    configTable = globalConfig.objects.all()
+    if configTable:
+        config = configTable[0]
+    else:
+        config=[]
+    template = loader.get_template('home/pages/login.html')
+    context = {
+        'config': config,
+    }
+    return HttpResponse(template.render(context, request))
+
